276a1cf4b8837878aa7866c1bd229fa83eab1973
Om Shah <omdshah2004@gmail.com>
2024-04-24 02:46:28 -0500

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Shah, Om on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
